23353ac2067d1441cf6597bb17444c9814f5509f
AmirGlz <amir.glz92@gmail.com>
2015-01-23 18:26:08 -0600

    Crud users

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :name, :user, :password, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+
   resources :books
 
   # The priority is based upon order of creation: first created -> highest priority.
